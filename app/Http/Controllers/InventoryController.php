6c70c041653322a32e4d894de641b81273275350
JRixGer <jrixcgeromo@gmail.com>
2018-10-23 21:25:58 +0800

    updates

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -33,70 +33,50 @@ class InventoryController extends Controller
                       ->get();
 
         $date_now_front = date('Y-m-d 00:00:00');
+        $date_now_front_num = strtotime(date('Y-m-d 00:00:00'));
+
         $inventory_date1_1 = strtotime('-1 day' , strtotime($date_now_front));
-        $inventory_date1_1 = date('Y-m-d H:i:s', $inventory_date1_1);
-
         $inventory_date7 = strtotime('-6 days' , strtotime($date_now_front));
-        $inventory_date7 = date('Y-m-d H:i:s', $inventory_date7);
-
         $inventory_date14 = strtotime('-13 days' , strtotime($date_now_front));
-        $inventory_date14 = date('Y-m-d H:i:s', $inventory_date14);
-
         $inventory_date30 = strtotime('-29 days' , strtotime($date_now_front));
-        $inventory_date30 = date('Y-m-d H:i:s', $inventory_date30);
-
         $date_2days = strtotime('-2 days' , strtotime($date_now_front)); 
-        $date_2days = date('Y-m-d H:i:s' , $date_2days); 
-
         $date_3days = strtotime('-3 days' , strtotime($date_now_front)); 
-        $date_3days = date('Y-m-d H:i:s' , $date_3days); 
-     
         $date_4days = strtotime('-4 days' , strtotime($date_now_front)); 
-        $date_4days = date('Y-m-d H:i:s' , $date_4days); 
-     
         $date_5days = strtotime('-5 days' , strtotime($date_now_front)); 
-        $date_5days = date('Y-m-d H:i:s' , $date_5days); 
-       
         $date_6days = strtotime('-6 days' , strtotime($date_now_front)); 
-        $date_6days = date('Y-m-d H:i:s' , $date_6days);
-       
         $date_7days = strtotime('-7 days' , strtotime($date_now_front)); 
-        $date_7days = date('Y-m-d H:i:s' , $date_7days);
-        
         $date_8days = strtotime('-8 days' , strtotime($date_now_front)); 
-        $date_8days = date('Y-m-d H:i:s' , $date_8days);
-      
         $date_9days = strtotime('-9 days' , strtotime($date_now_front)); 
-        $date_9days = date('Y-m-d H:i:s' , $date_9days);
-       
         $date_10days = strtotime('-10 days' , strtotime($date_now_front)); 
-        $date_10days = date('Y-m-d H:i:s' , $date_10days);
-        
         $date_11days = strtotime('-11 days' , strtotime($date_now_front)); 
-        $date_11days = date('Y-m-d H:i:s' , $date_11days);
-        
         $date_12days = strtotime('-12 days' , strtotime($date_now_front)); 
-        $date_12days = date('Y-m-d H:i:s' , $date_12days);
 
         //to
 
         $date_now2_front = date('Y-m-d 23:59:59');
-
         $inventory_date2_2 = strtotime('-1 day' , strtotime($date_now2_front));
-        $inventory_date2_2 = date('Y-m-d H:i:s', $inventory_date2_2);
-
         $inventory_date_7 = strtotime('-7 days' , strtotime($date_now2_front));
-        $inventory_date_7 = date('Y-m-d H:i:s', $inventory_date_7);
-
         $inventory_date_30 = strtotime('-30 days' , strtotime($date_now2_front));
-        $inventory_date_30 = date('Y-m-d H:i:s', $inventory_date_30);
+
+        $date_2_days = strtotime('-2 days' , strtotime($date_now2_front)); 
+        $date_3_days = strtotime('-3 days' , strtotime($date_now2_front)); 
+        $date_4_days = strtotime('-4 days' , strtotime($date_now2_front)); 
+        $date_5_days = strtotime('-5 days' , strtotime($date_now2_front)); 
+        $date_6_days = strtotime('-6 days' , strtotime($date_now2_front)); 
+        $date_7_days = strtotime('-7 days' , strtotime($date_now2_front)); 
+        $date_8_days = strtotime('-8 days' , strtotime($date_now2_front)); 
+        $date_9_days = strtotime('-9 days' , strtotime($date_now2_front)); 
+        $date_10_days = strtotime('-10 days' , strtotime($date_now2_front)); 
+        $date_11_days = strtotime('-11 days' , strtotime($date_now2_front)); 
+        $date_12_days = strtotime('-12 days' , strtotime($date_now2_front)); 
+
         
         DB::table('daily_ship')->delete();
         
         foreach ($invs as $key => $inv) 
         {
 
-            $qty4 = ($date_now_front >= strtotime($inv->notifications_date) && strtotime($inv->notifications_date) <= $date_now2_front? $inv->lineItems_quantity : 0);  
+            $qty4 = ($date_now_front_num >= strtotime($inv->notifications_date) && strtotime($inv->notifications_date) <= $date_now2_front? $inv->lineItems_quantity : 0);  
             $qty5 = ($inventory_date1_1 >= strtotime($inv->notifications_date) && strtotime($inv->notifications_date) <= $inventory_date2_2? $inv->lineItems_quantity: 0);        
             $qty7 = ($inventory_date7 >= strtotime($inv->notifications_date) && strtotime($inv->notifications_date) <= $date_now2_front? $inv->lineItems_quantity: 0); 
             $qty14 = ($inventory_date14 >= strtotime($inv->notifications_date) && strtotime($inv->notifications_date) <= $date_now2_front? $inv->lineItems_quantity: 0);        
