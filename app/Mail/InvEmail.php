6f660c2e9c1fcac474fc6ffc87ff2bb4b1927a23
JRixGer <jrixcgeromo@gmail.com>
2018-10-31 22:11:31 +0800

    updates

--- app/Mail/InvEmail.php
+++ app/Mail/InvEmail.php
@@ -6,6 +6,8 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use DB;
+use App\Sku;
 
 class InvEmail extends Mailable
 {
@@ -37,18 +39,149 @@ class InvEmail extends Mailable
      */
     public function build()
     {
+
+        //updateProd_fn();
+        $invs = DB::table('notifications')
+                      ->select(
+                            'notifications.dt as notifications_date',
+                            'lineItems.itemNo as lineItems_itemNo',
+                            'lineItems.productTitle as lineItems_productTitle',
+                            'lineItems.quantity as lineItems_quantity'
+                       )
+                      ->leftjoin('lineItems', 'lineItems.lnkid', '=', 'notifications.id')
+                      ->where('lineItems.itemNo', '<>', '')
+                      ->where('notifications.transactionType', '<>', 'TEST')
+                      ->where('notifications.transactionType', '<>', 'TEST_SALE')
+                      ->orderby('notifications.id', 'desc')
+                      ->get();
+
+
+        $date_now_front = date('n/j/Y', strtotime('now')).' 00:00:00';
+        $date_now_front_num = strtotime($date_now_front);
+
+
+        $inventory_date30 = strtotime('-30 days' , strtotime($date_now_front));
+        $date_1day = strtotime('-1 day' , strtotime($date_now_front)); 
+        $date_2days = strtotime('-2 days' , strtotime($date_now_front)); 
+        $date_3days = strtotime('-3 days' , strtotime($date_now_front)); 
+        $date_4days = strtotime('-4 days' , strtotime($date_now_front)); 
+        $date_5days = strtotime('-5 days' , strtotime($date_now_front)); 
+        $date_6days = strtotime('-6 days' , strtotime($date_now_front)); 
+        $date_7days = strtotime('-7 days' , strtotime($date_now_front)); 
+        $date_8days = strtotime('-8 days' , strtotime($date_now_front)); 
+        $date_9days = strtotime('-9 days' , strtotime($date_now_front)); 
+        $date_10days = strtotime('-10 days' , strtotime($date_now_front)); 
+        $date_11days = strtotime('-11 days' , strtotime($date_now_front)); 
+        $date_12days = strtotime('-12 days' , strtotime($date_now_front)); 
+        $date_13days = strtotime('-13 days' , strtotime($date_now_front)); 
+        $date_14days = strtotime('-14 days' , strtotime($date_now_front)); 
+
+
+        //to
+
+        $date_now2_front = date('n/j/Y', strtotime('now')).' 23:59:59';
+        $date_now2_front_num = strtotime($date_now_front);
+
+        $inventory_date_30 = strtotime('-30 days' , strtotime($date_now2_front));
+        $date_1_day = strtotime('-1 day' , strtotime($date_now2_front)); 
+        $date_2_days = strtotime('-2 days' , strtotime($date_now2_front)); 
+        $date_3_days = strtotime('-3 days' , strtotime($date_now2_front)); 
+        $date_4_days = strtotime('-4 days' , strtotime($date_now2_front)); 
+        $date_5_days = strtotime('-5 days' , strtotime($date_now2_front)); 
+        $date_6_days = strtotime('-6 days' , strtotime($date_now2_front)); 
+        $date_7_days = strtotime('-7 days' , strtotime($date_now2_front)); 
+        $date_8_days = strtotime('-8 days' , strtotime($date_now2_front)); 
+        $date_9_days = strtotime('-9 days' , strtotime($date_now2_front)); 
+        $date_10_days = strtotime('-10 days' , strtotime($date_now2_front)); 
+        $date_11_days = strtotime('-11 days' , strtotime($date_now2_front)); 
+        $date_12_days = strtotime('-12 days' , strtotime($date_now2_front)); 
+        $date_13_days = strtotime('-13 days' , strtotime($date_now2_front)); 
+        $date_14_days = strtotime('-14 days' , strtotime($date_now2_front)); 
+        
+        DB::table('daily_ship')->delete();
+  
+        foreach ($invs as $key => $inv) 
+        {
+
+            $qty30 = ((int)$inventory_date30 <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_now2_front_num? $inv->lineItems_quantity: 0);        
+            $qty01 = ((int)$date_1day <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_1_day? $inv->lineItems_quantity: 0);   
+            $qty02 = ((int)$date_2days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_2_days? $inv->lineItems_quantity: 0);   
+            $qty03 = ((int)$date_3days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_3_days? $inv->lineItems_quantity: 0);        
+            $qty04 = ((int)$date_4days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_4_days? $inv->lineItems_quantity: 0);        
+            $qty05 = ((int)$date_5days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_5_days? $inv->lineItems_quantity: 0);        
+            $qty06 = ((int)$date_6days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_6_days? $inv->lineItems_quantity: 0);        
+            $qty07 = ((int)$date_7days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_7_days? $inv->lineItems_quantity: 0); 
+            $qty08 = ((int)$date_8days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_8_days? $inv->lineItems_quantity: 0); 
+            $qty09 = ((int)$date_9days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_9_days? $inv->lineItems_quantity: 0); 
+            $qty10 = ((int)$date_10days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_10_days? $inv->lineItems_quantity: 0); 
+            $qty11 = ((int)$date_11days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_11_days? $inv->lineItems_quantity: 0); 
+            $qty12 = ((int)$date_12days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_12_days? $inv->lineItems_quantity: 0); 
+            $qty13 = ((int)$date_13days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_13_days? $inv->lineItems_quantity: 0);
+            $qty14 = ((int)$date_14days <= (int)strtotime($inv->notifications_date) && (int)strtotime($inv->notifications_date) <= (int)$date_14_days? $inv->lineItems_quantity: 0);
+
+            DB::table('daily_ship')->insert(
+            [
+                'item_number' => $inv->lineItems_itemNo, 
+                'description' => $inv->lineItems_productTitle, 
+                'qty01' => $qty01, 
+                'qty02' => $qty02, 
+                'qty03' => $qty03, 
+                'qty04' => $qty04, 
+                'qty05' => $qty05, 
+                'qty06' => $qty06, 
+                'qty07' => $qty07, 
+                'qty08' => $qty08, 
+                'qty09' => $qty09, 
+                'qty10' => $qty10,
+                'qty11' => $qty11, 
+                'qty12' => $qty12,
+                'qty13' => $qty13,
+                'qty14' => $qty14, 
+                'qty30' => $qty30
+            ]
+            );
+
+        }
+
+        $daily_ship = DB::table('daily_ship')
+                      ->selectRaw(
+                        'daily_ship.item_number AS item_number,
+                        daily_ship.description AS description,
+                        skus.prodQty AS prodQty, 
+                        SUM(daily_ship.qty01) AS qty01,
+                        SUM(daily_ship.qty02) AS qty02,
+                        SUM(daily_ship.qty03) AS qty03,
+                        SUM(daily_ship.qty04) AS qty04,
+                        SUM(daily_ship.qty05) AS qty05,
+                        SUM(daily_ship.qty06) AS qty06,
+                        SUM(daily_ship.qty07) AS qty07,
+                        SUM(daily_ship.qty08) AS qty08,
+                        SUM(daily_ship.qty09) AS qty09,
+                        SUM(daily_ship.qty10) AS qty10,
+                        SUM(daily_ship.qty11) AS qty11,
+                        SUM(daily_ship.qty12) AS qty12,
+                        SUM(daily_ship.qty12) AS qty13,
+                        SUM(daily_ship.qty04) AS qty4,
+                        SUM(daily_ship.qty05) AS qty5,
+                        SUM(daily_ship.qty07) AS qty7,
+                        SUM(daily_ship.qty14) AS qty14,
+                        SUM(daily_ship.qty30) AS qty30'
+                      )
+                      ->leftjoin('skus', 'skus.prodCode', '=', 'daily_ship.item_number')
+                      ->groupBy('daily_ship.item_number')
+                      ->groupBy('daily_ship.description')
+                      ->groupBy('skus.prodQty')
+                     ->get();
+
+
         return $this->from('sales@cb.preparedpatriot.us')
                     ->view('mails.inv')
                     //->text('mails.inv_plain')
-                    ->with(
-                      [
-                            'testVarOne' => '1',
-                            'testVarTwo' => '2',
-                      ])
-                      ->attach(public_path('/images').'/test.jpg', [
-                              'as' => 'test.jpg',
-                              'mime' => 'image/jpeg',
-                      ]);
+                    ->with('daily_ship_inv', $daily_ship);
+                    // ->attach(public_path('/images').'/test.jpg', [
+                    //           'as' => 'test.jpg',
+                    //           'mime' => 'image/jpeg',
+                    // ]);
     }
 
     // http://localhost/inv/mail/send       - to execute
--- resources/views/mails/inv.blade.php
+++ resources/views/mails/inv.blade.php
@@ -1,56 +1,318 @@
+<?php
+
+  $datenow = date('Y-m-d H:i:s');
+
+  $dt_last1 = strtotime('-1 day' , strtotime($datenow));
+  $dt_last1 = date('m/d', $dt_last1);
+
+  $dt_last2 = strtotime('-2 day' , strtotime($datenow));
+  $dt_last2 = date('m/d', $dt_last2);
+
+  $dt_last3 = strtotime('-3 day' , strtotime($datenow));
+  $dt_last3 = date('m/d', $dt_last3);
+
+  $dt_last4 = strtotime('-4 day' , strtotime($datenow));
+  $dt_last4 = date('m/d', $dt_last4);
+
+  $dt_last5 = strtotime('-5 day' , strtotime($datenow));
+  $dt_last5 = date('m/d', $dt_last5);
+
+  $dt_last6 = strtotime('-6 day' , strtotime($datenow));
+  $dt_last6 = date('m/d', $dt_last6);
+
+  $dt_last7 = strtotime('-7 day' , strtotime($datenow));
+  $dt_last7 = date('m/d', $dt_last7);
+
+  $dt_last8 = strtotime('-8 day' , strtotime($datenow));
+  $dt_last8 = date('m/d', $dt_last8);
+
+  $dt_last9 = strtotime('-9 day' , strtotime($datenow));
+  $dt_last9 = date('m/d', $dt_last9);
+
+  $dt_last10 = strtotime('-10 day' , strtotime($datenow));
+  $dt_last10 = date('m/d', $dt_last10);
+
+  $dt_last11 = strtotime('-11 day' , strtotime($datenow));
+  $dt_last11 = date('m/d', $dt_last11);
+
+  $dt_last12 = strtotime('-12 day' , strtotime($datenow));
+  $dt_last12 = date('m/d', $dt_last12);
+
+  $dt_last13 = strtotime('-13 day' , strtotime($datenow));
+  $dt_last13 = date('m/d', $dt_last13);
+
+  $dt_last14 = strtotime('-14 day' , strtotime($datenow));
+  $dt_last14 = date('m/d', $dt_last14);
+
+?>
 
 
-<div>
-<table class="table" border="1">
-  <thead>
-    <tr>
-      <th scope="col" colspan="16">Hello Sir,</th>
-    </tr>
-    <tr>
-      <th scope="col" colspan="16">Inventory Details</th>
-    </tr>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">receipt</th>
-      <th scope="col">Email</th>
-      <th scope="col">FirstName</th>
-      <th scope="col">LastName</th>
-      <th scope="col">OrderDate</th>
-      <th scope="col">SKU</th>
-      <th scope="col">QTY</th>
-      <th scope="col">ShipToName</th>
-      <th scope="col">ShipToAddress1</th>
-      <th scope="col">ShipToAddress2</th>
-      <th scope="col">ShipToCity</th>
-      <th scope="col">ShipToState</th>
-      <th scope="col">ShipToZip</th>
-      <th scope="col">ShipToCountry</th>
-      <th scope="col">Description</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>here</td>
-      <td>here</td>
-      <td>here</td>
-      <td>Otto</td>
-      <td>mdo</td>
-      <td>Otto</td>
-      <td>mdo</td>
-      <td>Otto</td>
-      <td>mdo</td>
-      <td>Otto</td>
-      <td>mdo</td>
-      <td>Otto</td>
-      <td>mdo</td>
-      <td>Otto</td>
-      <td>mdo</td>
-    </tr>
-    <tr>
-      <th scope="col" colspan="16">Thank you</th>
-    </tr>
-  </tbody>
-</table>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Inventory</title>  
+  <meta charset="utf-8" />  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+<script>
+  document.createElement("header"); 
+  document.createElement("nav");
+  document.createElement("footer");
+  document.createElement("section")
+</script>
+<style>
+*{ margin:0; padding:0}
+html, body{width:100%; min-height:100%;}
+a{ text-decoration:none;}
+li{ list-style-type:none;}
+img{border:0;}
+@font-face {
+  font-family: 'Conv_BPG DejaVu Sans ExtraLight 2012';
+  src: url('fonts/BPG DejaVu Sans ExtraLight 2012.eot');
+  src: local('☺'), url('fonts/BPG DejaVu Sans ExtraLight 2012.ttf') format('truetype'), url('../fonts/BPG DejaVu Sans ExtraLight 2012.svg') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'Conv_BPG DejaVu Sans ExtraLight Caps 2012';
+  src: url('fonts/BPG DejaVu Sans ExtraLight Caps 2012.eot');
+  src: local('☺'), url('fonts/BPG DejaVu Sans ExtraLight Caps 2012.ttf') format('truetype'), url('../fonts/BPG DejaVu Sans ExtraLight Caps 2012.svg') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
 
-</div>
+/*main css*/
+#wrapper{
+  width:95%;
+  max-width:95%;
+  padding:1%;
+  background:#1c75d7 url(../images/corner.png) no-repeat right bottom;
+  background-size:auto 100%;
+  margin:10px auto; 
+}
+#maincContent{
+  -webkit-box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.24);
+  -moz-box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.24);
+  box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.24);
+  background-color:#fff;
+  margin-bottom:13px;
+  position:relative;
+  clear:both;
+  overflow:hidden;
+  border-bottom-right-radius:10px;
+}
+#head{
+  width:95%;
+  max-width:95%;
+  margin:2px auto;
+}
+#head h1{
+  width:95%;
+  height:47px;
+  margin:2%; 
+
+}
+#head h1 a{
+  display:block;
+  text-indent:-9999px;
+  width:95%;
+  height:100%;
+  background: url(../images/logo.png) no-repeat center center;  
+}
+#content{
+  width:95%;
+  max-width:95%;
+  margin:1px auto;
+}
+#content h2{
+  color:#5c5c5c;
+  font-size:15px; 
+  margin-bottom:5px;
+  font-family: 'Conv_BPG DejaVu Sans ExtraLight 2012';
+  margin:1%; 
+}
+#content .parpagraph{
+  color:#5c5c5c;
+  font-size:15px; 
+  margin-bottom:5px;
+  font-family: 'Conv_BPG DejaVu Sans ExtraLight 2012';
+  font-weight:600;
+  margin:1%;
+}
+#content .parpagraphB{
+  color:#ooo;
+  font-size:14px; 
+  margin-bottom:5px;
+  font-family: 'Conv_BPG DejaVu Sans ExtraLight Caps 2012';
+  font-weight:600;
+  margin:1%;
+}
+#content img{
+  display:block;
+  margin:2%;
+}
+#maincContent::after {
+  content: "";
+  position: absolute;
+  z-index:2;
+  bottom: 0;
+  right: 0%;
+  width: 0px;
+  height: 0px;
+  border-top:35px solid #dbdbdb;
+  border-right: 35px solid #dbdbdb;
+}  
+#footer{
+  width:95%;
+  max-width:95%;
+  margin:0 auto;
+}
+#footer ul{
+  margin-left:7%;
+  overflow:hidden;  
+}
+#footer ul li{
+  display:inline-block;
+  float:left;
+  width:  31px;
+  height:35px;
+  margin-right:2%;
+}
+#footer ul li a{
+  display:block;
+  width:95%;
+  height:100%;
+  text-indent:-9999px;  
+  margin-bottom:8px;
+}
+#footer ul li a.mail{
+  background:url(../images/mail.png) no-repeat center center;
+}
+#footer ul li a.fb{
+  background:url(../images/fb.png) no-repeat center center;
+}
+#footer ul li a.link{
+  background:url(../images/link.png) no-repeat center center;
+}
+tr.border_bottom td {
+  border:1pt solid #cccccc;
+}
+.col {
+  font-weight: bold;
+}
+</style>
+</head>
+<body>
+  <section id="wrapper"><!--id="wrapper"-->
+      <section id="maincContent"><!--id="maincContent"-->
+          <header id="head"><!--header-->
+            <h1><a href="index.html" target="_blank">Inventory</a></h1>           
+          </header><!--End of header-->
+          <section id="content"><!--id="content"-->
+                <h2> Hello Sir Joe, </h2>
+                <p>Below is the inventory report for today: </p>
+                <div class="parpagraph" style="width:100%">
+
+                    <table class="table table-sm">
+ 
+                        <tr class="border_bottom">
+
+                          <td scope="col">CB SKU </td>
+
+                          <td scope="col">DESC</td>
+
+                          <td scope="col" style="color: rgb(0, 85, 255);">BAL (dummy for now)</td>
+
+                          <td scope="col" style="color: rgb(255, 0, 0);">30D</td>
+
+                          <td scope="col" style="color: rgb(255, 0, 0);">14D</td>
+
+                          <td scope="col">{{!!$dt_last1!!}}</td>
+
+                          <td scope="col">{{!!$dt_last2!!}}</td>
+
+                          <td scope="col">{{!!$dt_last3!!}}</td>
+
+                          <td scope="col">{{!!$dt_last4!!}}</td>
+
+                          <td scope="col">{{!!$dt_last5!!}}</td>
+
+                          <td scope="col">{{!!$dt_last6!!}}</td>
+
+                          <td scope="col">{{!!$dt_last7!!}}</td>
+
+                          <td scope="col" style="color: rgb(255, 0, 0);">7D</td>
+
+                          <td scope="col">{{!!$dt_last8!!}}</td>
+
+                          <td scope="col">{{!!$dt_last9!!}}</td>
+
+                          <td scope="col">{{!!$dt_last10!!}}</td>
+
+                          <td scope="col">{{!!$dt_last11!!}}</td>
+
+                          <td scope="col">{{!!$dt_last12!!}}</td>
+
+                          <td scope="col">{{!!$dt_last13!!}}</td>
+
+                          <td scope="col">{{!!$dt_last14!!}}</td>
+
+
+                        </tr>
+
+
+                            @foreach($daily_ship_inv as $row)
+
+                            <?php 
+                            $d07 = ($row->qty08+$row->qty09+$row->qty10+$row->qty11+$row->qty12+$row->qty13+$row->qty14);
+                            $d14 = ($row->qty01+$row->qty02+$row->qty03+$row->qty04+$row->qty05+$row->qty06+$row->qty07)+$d07; 
+                            ?>
+                            
+                            <tr class="border_bottom">
+                                <td>{{!! $row->item_number !!}}</td>
+                                <td>{{!! $row->description !!}}</td>
+                                <td>{{!! $row->prodQty - ($d14 + $d07) !!}}</td>
+                                <td>{{!! $row->qty30 !!}}</td>
+
+                                <td>{{!! $d14 !!}}</td>
+                                <td>{{!! $row->qty01 !!}}</td>
+                                <td>{{!! $row->qty02 !!}}</td>
+                                <td>{{!! $row->qty03 !!}}</td>
+                                <td>{{!! $row->qty04 !!}}</td>
+                                <td>{{!! $row->qty05 !!}}</td>
+                                <td>{{!! $row->qty06 !!}}</td>
+                                <td>{{!! $row->qty07 !!}}</td>
+
+                                <td>{{!! $d07 !!}}</td>
+                                <td>{{!! $row->qty08 !!}}</td>
+                                <td>{{!! $row->qty09 !!}}</td>
+                                <td>{{!! $row->qty10 !!}}</td>
+                                <td>{{!! $row->qty11 !!}}</td>
+                                <td>{{!! $row->qty12 !!}}</td>
+                                <td>{{!! $row->qty13 !!}}</td>
+                                <td>{{!! $row->qty14 !!}}</td>
+
+                              </tr>
+                            @endforeach
+                    </table>
+
+                </div>
+                <br>
+                <div class="parpagraphB">
+                 Thank you
+                </div>
+                <!-- <img src="../images/line.png" alt="#" /> -->
+          </section><!--End of id="content"-->
+           
+        </section><!--En dof id="maincContent"-->
+        <!-- <footer id="footer">
+          <ul>
+              <li><a href="#" target="_blank"  class="mail">mail</a></li>
+                <li><a href="#" target="_blank" class="fb">facebook</a></li>
+                <li><a href="#" target="_blank" class="link">link</a></li>
+            </ul>
+        </footer> -->
+    </section><!--End of id="wrapper"-->
+</body>
+</html>
